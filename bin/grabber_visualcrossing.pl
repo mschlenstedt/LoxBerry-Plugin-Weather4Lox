ae2dd32580743daa681ae1e3edaf1d6c529767ee
mschlenstedt <michael@schlenn.net>
2022-12-20 20:47:50 +0100

    Timezone problem with visualcrossing -> problem was that current data comes only every half an hour and therefore data for next hour sometimes was skipped in emulator data; fixed no icons in hourly forecast for OpenWeatherMap; fixes #81

--- bin/grabber_visualcrossing.pl
+++ bin/grabber_visualcrossing.pl
@@ -135,6 +135,7 @@ open(F,">$lbplogdir/current.dat.tmp") or $error = 1;
 		exit 2;
 	}
 	binmode F, ':encoding(UTF-8)';
+	my $currentepoche = $decoded_json->{currentConditions}->{datetimeEpoch}; # Needed during hourly forecast
 	print F "$decoded_json->{currentConditions}->{datetimeEpoch}|";
 	my $date = qx(date -R -d "\@$decoded_json->{currentConditions}->{datetimeEpoch}");
 	chomp ($date);
@@ -381,12 +382,11 @@ open(F,">$lbplogdir/hourlyforecast.dat.tmp") or $error = 1;
 		exit 2;
 	}
 	binmode F, ':encoding(UTF-8)';
-	my $now = time();
 	$i = 1;
 	for my $resultsdays ( @{$decoded_json->{days}} ){
 		for my $results( @{$resultsdays->{hours}} ){
 			# Skip first datasets of current day
-			if ($now >= $results->{datetimeEpoch}) {
+			if ($currentepoche >= $results->{datetimeEpoch}) {
 				next;
 			}
 			print F "$i|";
