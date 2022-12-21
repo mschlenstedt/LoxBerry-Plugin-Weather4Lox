e696a5ae01f466ea99a05b97c164c7b3218e94a0
mschlenstedt <michael@schlenn.net>
2022-12-21 05:43:18 +0100

    Backup custom theme files; fixes #77

--- postupgrade.sh
+++ postupgrade.sh
@@ -13,6 +13,9 @@ cp -p -v -r /tmp/$ARGV1\_upgrade/config/$ARGV3/* $ARGV5/config/plugins/$ARGV3/
 echo "<INFO> Copy back existing log files"
 cp -p -v -r /tmp/$ARGV1\_upgrade/log/$ARGV3/* $ARGV5/log/plugins/$ARGV3/ 
 
+echo "<INFO> Copy back custom theme files"
+cp -p -v -r /tmp/$ARGV1\_upgrade/themes/$i $ARGV5/templates/plugins/$ARGV3/themes
+
 echo "<INFO> Remove temporary folders"
 rm -r /tmp/$ARGV1\_upgrade
 
