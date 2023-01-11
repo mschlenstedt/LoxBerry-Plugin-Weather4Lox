e9d99c996fdac9054cfc55676dae689efadbb820
Manuel <mr-manuel@outlook.it>
2023-01-11 17:09:18 +0100

    Corrected spaces

--- bin/grabber_wu.pl
+++ bin/grabber_wu.pl
@@ -3,7 +3,7 @@
 # Grabber for overwriting data by WeatherUnderground data
 
 # Copyright 2016-2023 Michael Schlenstedt, michael@loxberry.de
-# 			          Christian Fenzl, christian@loxberry.de
+#                     Christian Fenzl, christian@loxberry.de
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
